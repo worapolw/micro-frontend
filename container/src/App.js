7ec90566cde1390578b7700649a97c8ff3bd63a0
Worapol Worakunsap <worapolworakunsap@ip-192-168-1-107.ap-southeast-1.compute.internal>
2022-11-24 04:21:40 +0700

    Initial commit

---
+++ container/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+// import { mount } from "marketing/MarketingApp";
+
+// console.log(mount);
+
+export default () => {
+  return (
+    <div>
+      <h1>Hi There!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
